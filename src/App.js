ec7987f413599a9674dec99346f79cccdbf788c5
Lex van der Werff <dev@lexvanderwerff.com>
2021-08-27 11:43:59 +0200

    404 added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import Layout from "components/layout/Layout";
 import { lazy, Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 
-const SomePage = lazy(() => import("./pages/SomePage"))
+const SomePage = lazy(() => import("./pages/SomePage"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 const App = () => {
 	return (
@@ -14,6 +15,9 @@ const App = () => {
 					</Route>
 					<Route path="/somethings">
 						<SomePage />
+					</Route>
+					<Route path='*'>
+						<NotFound />
 					</Route>
 				</Switch>
 			</Suspense>
