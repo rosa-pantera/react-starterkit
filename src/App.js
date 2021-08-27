7c4e5ad1d601d0aacfb9d2cfefd8b461b27017e4
Lex van der Werff <dev@lexvanderwerff.com>
2021-08-27 10:41:52 +0200

    Added lazy loading
    Added Lazy Loading.
    Added Layout component.
    Added store.
    Added router.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import Layout from "components/layout/Layout";
+import { lazy, Suspense } from "react";
+import { Route, Switch } from "react-router-dom";
+
+const SomePage = lazy(() => import("./pages/SomePage"))
+
 const App = () => {
 	return (
-		<p>hi</p>
+		<Layout>
+			<Suspense fallback={<p>Loading...</p>}>
+				<Switch>
+					<Route path="/" exact>
+						<p>Homepage!</p>
+					</Route>
+					<Route path="/somethings">
+						<SomePage />
+					</Route>
+				</Switch>
+			</Suspense>
+		</Layout>
 	);
 }
 
