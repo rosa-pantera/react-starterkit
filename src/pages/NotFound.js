bf9c468b46d5ff6ad5982df7314b980e4ab2d0ce
Lex van der Werff <dev@lexvanderwerff.com>
2021-08-29 16:47:23 +0200

    Update NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
 	return (
 		<div className="bg-white min-h-screen px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
@@ -10,18 +12,12 @@ const NotFound = () => {
 							<p className="mt-1 text-base text-gray-500">Please check the URL in the address bar and try again.</p>
 						</div>
 						<div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
-							<a
-								href="#"
+							<Link
+								to="/"
 								className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 							>
 								Go back home
-							</a>
-							<a
-								href="#"
-								className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-							>
-								Contact support
-							</a>
+							</Link>
 						</div>
 					</div>
 				</main>
